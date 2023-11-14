75ec743fd558cda8c53549e5df741dc087827e67
Graham Booth <gbooth@c4b12c6.42berlin.de>
2023-11-14 10:11:12 +0100

    Revert "api (add_game_data and get_game_data) working from front end"
    This reverts commit 1407565353f12b2e0878a1e0fe818cc888961428.

--- Makefile
+++ Makefile
@@ -33,10 +33,7 @@ test_add_game_data:
 test_add_game_data_json:
 	curl -X POST -H "Content-Type: application/json" -d '{"player1_name": "John", "player1_points": 10, "player2_name": "Jane", "player2_points": 8}' http://localhost:8000/api/add_game_data/
 
-test_get_existing_game_data:
+test_get_game_data:
 	curl -X GET http://localhost:8000/api/get_game_data/1/
-
-test_get_nonexistant_game_data:
-	curl -X GET http://localhost:8000/api/get_game_data/1000/		
 
 .PHONY: all up down re clean prune exec_backend exec_backend_db exec_frontend cp_example test_add_game_data test_add_game_data_json test_get_game_data
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,8 @@
-#import logging
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.http import Http404
 from .models import GameData
-
-#logger = logging.getLogger(__name__)
 
 @csrf_exempt
 def add_game_data(request):
@@ -12,6 +10,7 @@ def add_game_data(request):
         try:
             # Attempt to parse the JSON content from the request body
             data = json.loads(request.body)
+            print("Received data:", data)
 
             # Extract data from the JSON object
             player1_name = data.get('player1_name')
@@ -19,7 +18,7 @@ def add_game_data(request):
             player2_name = data.get('player2_name')
             player2_points = data.get('player2_points')
 
-            if player1_name is not None and player1_points is not None and player2_name is not None and player2_points is not None:
+            if player1_name and player1_points and player2_name and player2_points:
                 game_data = GameData(
                     player1_name=player1_name,
                     player1_points=player1_points,
@@ -38,7 +37,10 @@ def add_game_data(request):
 def get_game_data(request, id):
     if request.method == 'GET':
         try:
+            # Attempt to retrieve the GameData record by its ID
             game_data = GameData.objects.get(id=id)
+
+            # Create a dictionary to represent the data
             data = {
                 'id': game_data.id,
                 'player1_name': game_data.player1_name,
@@ -46,8 +48,12 @@ def get_game_data(request, id):
                 'player2_name': game_data.player2_name,
                 'player2_points': game_data.player2_points,
             }
+
             return JsonResponse(data, status=200)
         except GameData.DoesNotExist:
-            return JsonResponse({'message': 'record with the given ID does not exist'}, status=400)
+            # If the record with the given ID does not exist
+            raise Http404("Game data does not exist")
+
+    # If the request method is not GET
     return JsonResponse({'message': 'GET request required'}, status=400)
 
